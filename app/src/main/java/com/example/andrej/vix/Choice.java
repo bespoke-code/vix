46f461c0a2208e2083ae3f62733c94d1722941d1
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2017-04-29 17:02:07 +0200

    By @angelazaff - commiting the initial android project.

---
+++ app/src/main/java/com/example/andrej/vix/Choice.java
@@ -0,0 +1,13 @@
+package com.example.andrej.vix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+    }
+}
