fe625a1df51a653f474c84f59ad250f76fe34940
Andrej Georgievski <andrej.zgeorgievski@gmail.com>
2017-04-29 15:26:29 +0200

    Added json to CSV functionality template

---
+++ pyvix/src/jsonToCSV.py
@@ -0,0 +1,16 @@
+import json
+import csv
+import parser
+
+def init():
+    pass
+
+def jsonToCSV(jsonFile):
+    Pass
+
+def saveJSON(json):
+    pass
+
+if __name__ == '__main__':
+    jsonToCSV()
+    saveJSON()
